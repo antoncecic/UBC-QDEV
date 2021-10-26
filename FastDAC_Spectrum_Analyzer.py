5cb3715144d9677c803eab39f31a57f6bf5dc167
Anton Cecic <85712185+antoncecic@users.noreply.github.com>
2021-10-26 12:17:44 -0700

    Update FastDAC_Spectrum_Analyzer.py

--- FastDAC_Spectrum_Analyzer.py
+++ FastDAC_Spectrum_Analyzer.py
@@ -1,6 +1,7 @@
 # FastDAC Spectrum Analyzer
 # by Anton Cecic
 
+from re import template
 import time
 import serial
 import numpy as np
@@ -11,12 +12,12 @@ import plotly.express as px
 import dash
 from dash import html
 from dash import dcc
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-def PSD(port, duration, channels=[0, ]):
+def PSD(port, baudrate, duration, channels=[0, ]):
 
-    s = serial.Serial(port, 1750000, timeout=1)        # Fibre optic connection
-    #s = serial.Serial('COM6', 57600, timeout=1)        # USB connection
+    s = serial.Serial(port, baudrate, timeout=1)        # Fibre optic connection
 
     def Query(command):
 
@@ -56,8 +57,8 @@ def PSD(port, duration, channels=[0, ]):
     channel_readings = {ac: list() for ac in channels}
     voltage_readings = []
     try:
-        while s.in_waiting > 24 or len(voltage_readings) <= num_bytes/2:
-            buffer = s.read(24)
+        while s.in_waiting > 240 or len(voltage_readings) <= num_bytes/2:
+            buffer = s.read(240)
             info = [buffer[i:i+2] for i in range(0, len(buffer), 2)]
             for two_bytes in info:
                 int_val = int.from_bytes(two_bytes, 'big')
@@ -82,21 +83,25 @@ def PSD(port, duration, channels=[0, ]):
 
 X = [[],[],[],[]]
 Y = [[],[],[],[]]
-PORT = [0,'COM4']
+PORT = [0,'COM3']
+BR = [0,1750000]
 DUR = [0,1.5]
 SELAVG = [0, 5]
 SELAX = [0, 'log']
 CHNL = [0, [0]]
 
-app = dash.Dash(__name__, title='FastDAC Spectrum Analyzer', update_title='Loading...')
-#app.scripts.config.serve_locally = True
+#app = dash.Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 
-app.layout = html.Div([
+app.layout = html.Div(
+    [
 
     html.Div([
 
         html.Div([
-            dcc.Graph(id="live-graph", animate=True)
+            dcc.Graph(id="live-graph", 
+                animate=True)
+
             ], style={'width': '75%', 
             'height':'100%', 
             'margin-left': '15px', 
@@ -105,163 +110,314 @@ app.layout = html.Div([
             'border': '3px black solid'}
             ),
 
-            dcc.Interval(id="graph-update", interval=1000, n_intervals=0)]),
+            dcc.Interval(id="graph-update", 
+                interval=1500, 
+                n_intervals=0)]),
 
-    html.Div([
+    dbc.Card([
 
-        html.Div([ 
-        html.Label(['Port:'], style={'font-weight': 'bold', 'margin-left': '15px','margin-right':'56px'}),
-        dcc.Input(id='enter-port', type='text', value=str(PORT[-1]), style={'width': '40%', 'height':'50%'})
-        ], style={'margin-top':'15px'}),
+        dbc.ListGroup(
+            [ 
+                dbc.Label(
+                    ['Port:'], color = '#1e81b0'
+                ),
+                dbc.Input(
+                    id='enter-port', 
+                    type='text', 
+                    value=str(PORT[-1]), 
+                    style={'color':'white'}
+                )
+            ],
+            flush=True,
+            style={'margin-top':'15px', 'margin-left': '15px', "margin-right": "15px"}
+        ),
 
-        html.Div([
-        html.Label(children=['Duration (s): '], style={'font-weight': 'bold', "text-align": "right","offset":0, 'margin-left': '15px'}),
-        dcc.Input(id='enter-duration', type = 'text',value=str(DUR[-1]), style={'width': '40%', "margin-bottom": "10px"})
-        ]),
+        dbc.ListGroup(
+            [
+                dbc.Label('Connection:', color = "#1e81b0"),
+                dbc.RadioItems(
+                    id='usb-checklist', 
+                    options=[
+                        {'label': 'USB', 'value': 57600},
+                        {'label': 'Fibre Optic', 'value': 1750000},
+                    ], 
+                    value = 1750000
+                    
+                ),
+            ], style = {'margin-top':'15px', 'margin-left': '15px', "margin-right": "15px"}, flush=True
+        ),
 
-        html.Div([
-        html.Label(['Show channels: '], style={'font-weight': 'bold', "text-align": "right","offset":0, 'display': 'inline-block', 'margin-left': '15px'}),
-        dcc.Checklist(id='channels-checklist', 
+        dbc.ListGroup(
+            [ 
+                dbc.Label(
+                    ['Duration:'], color = '#1e81b0'
+                ),
+                dbc.Input(
+                    id='enter-duration', 
+                    type='text', 
+                    value=str(DUR[-1]), 
+                    style={'color':'white'}
+                )
+            ],flush=True,
+            style={'margin-top':'15px', 'margin-left': '15px',"margin-right": "15px"}
+        ),
+
+        dbc.ListGroup(
+            [
+                dbc.Label(
+                    ['Show channels: '], color = '#1e81b0'
+                ),
+
+            dbc.Checklist(
+                id='channels-checklist', 
                 options=[
                     {'label': '0', 'value': 0},
                     {'label': '1', 'value': 1},
                     {'label': '2', 'value': 2},
-                    {'label': '3', 'value': 3}], 
+                    {'label': '3', 'value': 3}
+                ], 
+                    
                 value=[0],
-                style={"margin-bottom": "10px", 'display': 'inline-block','margin-left': '15px','margin-right': '10px'}),
-        ]),
+                
+            ),
+        ]  ,flush=True , style={'margin-top':'15px', 'margin-left': '15px',"margin-right": "15px"}
+    ),
 
-        html.Div([
-        dcc.Dropdown(
-                id='avg-dropdown', 
+        dbc.ListGroup(
+            [
+            dbc.RadioItems(
+                id='avg-checklist', 
                 options=[
-                    {'label': 'Average over 1 cycle:', 'value': 1},
-                    {'label': 'Average over 2 cycles:', 'value': 2},
-                    {'label': 'Average over 3 cycles:', 'value': 3},
-                    {'label': 'Average over 4 cycles:', 'value': 4},
-                    {'label': 'Average over 5 cycles:', 'value': 5},
-                    {'label': 'Average over 6 cycles:', 'value': 6}],
+                    {'label': 'Average over 0 cycles', 'value': 0},
+                    {'label': 'Average over 1 cycles', 'value': 1},
+                    {'label': 'Average over 2 cycles', 'value': 2},
+                    {'label': 'Average over 3 cycles', 'value': 3},
+                    {'label': 'Average over 4 cycles', 'value': 4},
+                    {'label': 'Average over 5 cycles', 'value': 5},
+                    {'label': 'Average over 6 cycles', 'value': 6},
+                    {'label': 'Average over 7 cycles', 'value': 7}
+                ], 
+
+                
+                    
                 value=5,
-                style={ "margin-bottom": "15px", 'width':'90%','margin-left': '10px','margin-right': '10px'}),
-        ]),
+                
+            ),
+        ] , style={'margin-top':'15px', 'margin-left': '15px', "margin-right": "15px"}, flush=True
+    ),
 
-        html.Div([
-            dcc.Dropdown(
-                id='axes-dropdown', 
-                options=[
-                    {'label': 'Log Axis', 'value': 'log'},
-                    {'label': 'Linear Axis', 'value': 'linear'}], 
-                value='log', 
-                style={ "margin-bottom": "15px", 'margin-left': '10px','margin-right': '10px',  'width':'90%'}),
-        ]),
+        dbc.ListGroup(
+            [
+                dbc.Label('Vertical axes:', color = "#1e81b0"),
+                dbc.RadioItems(
+                    id='axes-checklist', 
+                    options=[
+                        {'label': 'log', 'value': 'log'},
+                        {'label': 'linear', 'value': 'lin'},
+                    ], 
+                    value = 'log'
+                    
+                ),
+            ], style = {'margin-top':'15px', 'margin-left': '15px', "margin-right": "15px"}, flush=True
+        ),
 
-        html.Div([html.Button('OK', id='button', n_clicks=0)], 
-        style={'text-align':'center',"margin-bottom": "10px" })
+        dbc.ListGroup(
+            [
+            dbc.Button('OK', 
+                id='button', 
+                n_clicks=0)], 
+                style = {'margin-top':'15px', 'margin-left': '15px', "margin-right": "15px", "margin-bottom": "15px"},
+                flush=True,
+                )
+            ],
+            style = {
+                'width':'25%',
+                'height':'25%',
+                'margin-left':'15px', 
+                'display': 'inline-block'}
+        ),
 
-        ], style = {'width':'25%',
-            'height':'25%',
-            'border': '3px black solid', 
-            'backgroundColor':'white',
-            'margin-left':'15px', 
-            'display': 'inline-block'}),
+    dbc.Card(
+        [
 
-        html.Div([
+        dbc.ListGroup(
+            [
+            dbc.Label(
+                'Message: ', 
+                color="#1e81b0",
+                style={
+                    'margin-right':'10px'}),
+
+            dbc.Label(
+                children='-', 
+                id = 'label0')],
+                style={
+                    "margin-top": "15px", 
+                    "margin-left": "15px", 
+                    "margin-right": "15px"}, flush=True
+        ),
+
+        dbc.ListGroup(
+            [
+            dbc.Label(
+                'FastDAC ID: ', 
+                color="#1e81b0",
+                style={
+                    'margin-right':'15px'}),
+
+            dbc.Label(
+                children='-', 
+                id = 'label1')
+                ], flush=True,
+                style={
+                "margin-left": "15px", 
+                "margin-right": "15px"}
+        ),
+
+        dbc.ListGroup(
+            [
+            dbc.Label(
+                'Runtime (s): ', 
+                color="#1e81b0",
+                style={
+                    'margin-right':'15px'}
+                    ),
+
+            dbc.Label(
+                children='-', 
+                id = 'label2')
+            ],  
+            style={
+                "margin-left": "15px", 
+                "margin-right": "15px"}, flush=True
+        ),
+
+        dbc.ListGroup(
+            [
             
-            html.Div([html.Label('FastDAC ID: ', style={'font-weight': 'bold', 'margin-right':'10px'}),
-                    html.Label(children='-', id = 'label1')], 
-                    style={"margin-top": "15px", 
-                    "margin-left": "15px", 
-                    "margin-right": "15px"}),
-
-            html.Div([html.Label('Runtime (s): ', style={'font-weight': 'bold', 'margin-right':'15px'}),
-                html.Label(children='-', id = 'label2')], 
+            dbc.Label(
+                'bytes / cycle / channel: ', 
+                color="#1e81b0",
                 style={
-                    "margin-top": "15px",  
-                    "margin-bottom": "15px", 
-                    "margin-left": "15px", 
-                    "margin-right": "15px"}),
+                'margin-right':'15px'}
+                ),
 
-            html.Div([html.Label('bytes / cycle / channel: ', style={'font-weight': 'bold', 'margin-right':'15px'}),
-                html.Label(children='-', id = 'label3', style={'textAlign': 'left', 'margin-right':'15px'})], 
-                style={
-                    "margin-top": "15px",  
-                    "margin-bottom": "15px", 
-                    "margin-left": "15px", 
-                    "margin-right": "15px"}),
+            dbc.Label(
+                children='-', 
+                
+                id = 'label3', 
+                style={'textAlign': 'left', 
+                'margin-right':'15px'}
+                )
+            ], flush=True,
 
-            ], style={'vertical-align':'top', 
-                'border': '3px black solid', 
-                'backgroundColor':'white',
+            style={
+                "margin-left": "15px", 
+                "margin-right": "15px",
+                }),
+
+            ], style={
+                'vertical-align':'top', 
                 'display': 'inline-block', 
                 'margin-left':'15px', 
                 'height':'35%', 
                 'width':'48.5%'})
 
-    ], style={'backgroundColor':'#BFBAB9', 
+    ], style={
+        #'backgroundColor':'#BFBAB9', 
         'padding-top':'1%', 
         'padding-bottom':'50%',
         'padding-right':'0%',
-        'padding-left':'0%'})
+        'padding-left':'0%'}
+        
+)
 
 @app.callback(
     Output(component_id='live-graph', component_property='figure'),
     Output(component_id='graph-update', component_property='interval'),
+    Output(component_id = 'label0', component_property='children'),
     Output(component_id = 'label1', component_property='children'),
     Output(component_id = 'label2', component_property='children'),
     Output(component_id = 'label3', component_property='children'),
     [Input(component_id='graph-update', component_property='n_intervals'),
-    Input(component_id='avg-dropdown', component_property='value'),
-    Input(component_id='axes-dropdown', component_property='value'),
+    Input(component_id='usb-checklist', component_property='value'),
+    Input(component_id='avg-checklist', component_property='value'),
+    Input(component_id='axes-checklist', component_property='value'),
     Input(component_id='channels-checklist', component_property='value'),
     Input(component_id='button', component_property='n_clicks'),
     State(component_id='enter-port', component_property='value'),
     State(component_id='enter-duration', component_property='value')]
     )
 
-def update_graph(input_data, selected_avg, selected_axes, channel_arr, n_clicks, port, dur):
+def update_graph(input_data, baudrate, selected_avg, selected_axes, channel_arr, n_clicks, port, dur):
 
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
 
     if 'button' in changed_id:
         PORT.append(port)
+        BR.append(baudrate)
         DUR.append(dur)
         SELAVG.append(selected_avg)
         SELAX.append(selected_axes)
         CHNL.append(channel_arr)
-        
-    psd = PSD(str(PORT[-1]), float(DUR[-1])*2/3, CHNL[-1])
+        msg = 'loading...'
+    else:
+        msg = '-'
+
+    psd = PSD(str(PORT[-1]), str(BR[-1]), float(DUR[-1])*2/3, CHNL[-1])
 
     fig = make_subplots(rows=[1,2,2,2][len(CHNL[-1])-1], cols=[1,1,2,2][len(CHNL[-1])-1])
-    fig.update_layout(title_text="FastDAC Spectrum Analyzer", title_x=0.5, legend_title = "channels")
-    fig.update_yaxes(type=SELAX[-1], title_text='Potential [mV]')
+    fig.update_layout(title_text="FastDAC Spectrum Analyzer", title_x=0.5, legend_title = "channels", template='plotly_dark')
+    fig.update_yaxes(type=SELAX[-1], title_text='mV*mV / Hz')
     fig.update_xaxes(title_text='Frequency [Hz]')
     fig.update_layout(showlegend=False)
 
     for k in range(0, len(CHNL[-1])):
         X[k].append(psd[0][k][0])
         Y[k].append(psd[1][k][0])
+
         
         if len(X[k])<SELAVG[-1]:
             xnew=np.mean(X[k][-len(X[k]):-1], axis=0)
             ynew=np.mean(Y[k][-len(X[k]):-1], axis=0)
 
+            fig.add_trace(
+                go.Scatter(
+                    x=xnew[15:], 
+                    y=ynew[15:], 
+                    name=str(CHNL[-1][k])), 
+                    row=[1,2,1,2][k],
+                    col=[1,1,2,2][k])
+
         elif SELAVG[-1]==1:
             xnew=psd[0][k][0]
             ynew=psd[1][k][0]
+
+            fig.add_trace(
+                go.Scatter(
+                    x=xnew[15:], 
+                    y=ynew[15:], 
+                    name=str(CHNL[-1][k])), 
+                    row=[1,2,1,2][k],
+                    col=[1,1,2,2][k])
             
+        elif len(X[k][-1]) != len(X[k][-SELAVG[-1]]):
+            msg = 'loading...'
+
         else:
             xnew=np.mean(X[k][-SELAVG[-1]:-1], axis=0)
             ynew=np.mean(Y[k][-SELAVG[-1]:-1], axis=0)
+            msg = '-'
 
-        fig.add_trace(
-            go.Scatter(
-                x=xnew[15:], 
-                y=ynew[15:], 
-                name=str(CHNL[-1][k])), 
-                row=[1,2,1,2][k],
-                col=[1,1,2,2][k])
+            fig.add_trace(
+                go.Scatter(
+                    x=xnew[15:], 
+                    y=ynew[15:], 
+                    name=str(CHNL[-1][k])), 
+                    row=[1,2,1,2][k],
+                    col=[1,1,2,2][k])
 
-    return fig, 1500*float(dur), psd[4], psd[3], psd[2]
+    return fig, 1500*float(dur), msg, psd[4], psd[3], psd[2]
 
 if __name__ == '__main__':
-     app.run_server(host= '0.0.0.0', debug=False)
+     app.run_server(host= '0.0.0.0', debug=True)
