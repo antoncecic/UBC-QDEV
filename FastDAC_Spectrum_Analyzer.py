b264997f3fee924cd6659a416a9f07e94a49c0fd
Anton Cecic <85712185+antoncecic@users.noreply.github.com>
2021-10-26 12:24:14 -0700

    Update FastDAC_Spectrum_Analyzer.py

--- FastDAC_Spectrum_Analyzer.py
+++ FastDAC_Spectrum_Analyzer.py
@@ -1,5 +1,18 @@
-# FastDAC Spectrum Analyzer
-# by Anton Cecic
+# FASTDAC SPECTRUM ANALYZER
+# By Anton Cecic
+
+# This program allows a user to analyze the power spectral density of a FastDAC's ADC channels.  
+# The code is written in Python and uses a Dash (by Plotly) based interface with built in controls over various input parameters.
+
+# INSTRUCTIONS
+
+# 1. Download and run FastDAC_Spectrum_Analyzer.py
+# 2. The output will let you know that Dash is running and where to access it (ex: http://10.17.173.64:8050/).
+# 3. To kill the program, type CTRL+C in terminal.
+
+# If you plan on accessing the interface on a different device, both the computer and the device 
+# must be connected to the same network (ex: Ampel 111 network).  You cannot use the interface on more than one device at a time.
+
 
 from re import template
 import time
